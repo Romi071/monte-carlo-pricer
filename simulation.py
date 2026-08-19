f63e7c28a4f1e27d0ce7ef6ab825c7eaebdf9993
Romi <romi.mateos2005@gmail.com>
2026-08-19 12:35:59 +0200

    added initial cond variables

--- simulation.py
+++ simulation.py
@@ -15,5 +15,11 @@ log_prices = np.log(prices_arr[1:] / prices_arr[:-1])
 mean = 365*np.mean(log_prices)
 std = (365**0.5)*np.std(log_prices)
 
-print(mean)
-print(std)
+#Initial cond
+s0 = prices_arr[-1]
+iterations = 10000
+t_intervals = 30
+dt = 1/365
+
+#Z matrix for our random normal distribution
+Z_mat = np.random.normal(0, 1)
