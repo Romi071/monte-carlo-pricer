6648b24c9322c4420077676426ec500c205b673c
Romi <romi.mateos2005@gmail.com>
2026-08-22 23:32:21 +0200

    Project finished!

--- simulation.py
+++ simulation.py
@@ -16,6 +16,8 @@ log_prices = np.log(prices_arr[1:] / prices_arr[:-1])
 mean = 365*np.mean(log_prices)
 std = (365**0.5)*np.std(log_prices)
 
+print(mean)
+
 #Initial cond
 s0 = prices_arr[-1]
 iterations = 10000
@@ -33,9 +35,15 @@ sim[0, :] = s0
 for i in range(1, t_intervals + 1):
     sim[i, :] = sim[i-1, :]*np.exp((mean - 0.5*std**2)*dt + std*dt**0.5*Z_mat[i-1, :])
 
+#Probability the asset ends higher than it started:
+sim_bool = sim[-1, :] > s0 
+wins = sim_bool.sum()
+avgwin_amount = sim[-1, :].mean() - s0
+win_prob = 100 * wins / iterations
+print(f"There is a {win_prob}% chance that the asset ends higher than it started after 30 days")
+print(f"There average net balance after 30 days is {avgwin_amount:.2f}$")
 
 #Sim complete, now plot it:
-
 plot_data = sim[:, 0:100]
 histogram_data = sim[-1, :]
 
